71e9b63f2c39e2b93bc570edc4214f4910b876f3
Philippe Muller <philippe.muller@gmail.com>
2013-10-27 15:37:24 +0100

    Use vopen() to make DictFile works when reading remote repository meta data

--- ipkg/utils.py
+++ ipkg/utils.py
@@ -2,6 +2,8 @@ import os
 import json
 import logging
 
+from .vfiles import vopen
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -17,8 +19,7 @@ class DictFile(dict):
     def reload(self):
         if os.path.isfile(self.__file_path):
             LOGGER.debug('Loading %s', self.__file_path)
-            with open(self.__file_path) as f:
-                self.update(json.load(f))
+            self.update(json.load(vopen(self.__file_path)))
 
     def clear(self):
         """Force the dictionary to be empty.
@@ -29,5 +30,6 @@ class DictFile(dict):
 
     def save(self):
         LOGGER.debug('Writing %s', self.__file_path)
+        # This will break if trying to call save() on a remote DictFile
         with open(self.__file_path, 'w') as f:
             json.dump(self, f, indent=4)

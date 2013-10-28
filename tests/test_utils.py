d1e883529dd2d442095510bb5509c1e92f9b6c8a
Philippe Muller <philippe.muller@gmail.com>
2013-10-28 21:09:07 +0100

    Tests for utils.execute

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from shutil import rmtree
 from os.path import join
 import json
 
-from ipkg.utils import DictFile, InvalidDictFileContent
+from ipkg.utils import *
 
 
 class TestDictFile(TestCase):
@@ -52,3 +52,35 @@ class TestDictFile(TestCase):
         df1.save()
         df2 = DictFile(self.filepath)
         self.assertEqual(df2['truth'], 42)
+
+
+class TestExecute(TestCase):
+
+    def test(self):
+        execute('echo -n')
+
+    def test_get_stdout_as_string(self):
+        out = execute('echo 42', stdout=PIPE)[0]
+        self.assertEqual(out, '42\n')
+
+# FIXME: Why does it fails?!
+#    def test_get_stderr_as_string(self):
+#        err = execute('echo 42', stderr=PIPE)
+#        self.assertEqual(err, '42\n')
+
+    def test_exit_code_not_0(self):
+        with self.assertRaises(ExecutionFailed):
+            execute('ls /I-HOPE-THIS-FILE-WILL-NEVER-EXISTS', stderr=PIPE)
+
+    def test_command_not_found(self):
+        with self.assertRaises(ExecutionFailed):
+            execute('I-HOPE-THIS-COMMAND-WILL-NEVER-EXISTS', stderr=PIPE)
+
+    def test_with_data(self):
+        out = execute('cat', data='42', stdout=PIPE)[0]
+        self.assertEqual(out, '42')
+
+    def test_with_env(self):
+        out = execute(['sh', '-c', 'echo $FOO'],
+                      env={'FOO': '42'}, stdout=PIPE)[0]
+        self.assertEqual(out, '42\n')

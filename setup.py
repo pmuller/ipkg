10d18b41c863ad1fe5bf3726315653aaa7483c7e
Philippe Muller <philippe.muller@gmail.com>
2013-11-02 18:23:12 +0100

    removed support for python 3 I have things to learn first !

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
         'Topic :: System :: Installation/Setup',

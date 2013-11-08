5ee49da9686ee5418e9d68243a0753ee7a5a4795
Philippe Muller <philippe.muller@gmail.com>
2013-11-08 21:51:32 +0100

    updated IpkgException to ease message formatting
    not sure its a good idea...

--- ipkg/exceptions.py
+++ ipkg/exceptions.py
@@ -1,2 +1,21 @@
 class IpkgException(Exception):
     """Base exception for ipkg."""
+
+    MESSAGE = None
+
+    def __init__(self, *args):
+        self.args = args
+
+    def __str__(self):
+        if self.MESSAGE:
+            if self.args:
+                try:
+                    return self.MESSAGE % self.args
+                except TypeError:
+                    pass
+            return self.MESSAGE
+        else:
+            if self.args:
+                return str(self.args)
+            else:
+                return ''

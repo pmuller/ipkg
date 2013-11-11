afbe731965c4aa900f242724ea69ee656ba98825
Philippe Muller <philippe.muller@gmail.com>
2013-11-11 23:57:55 +0100

    manage platform requirement

--- ipkg/requirements.py
+++ ipkg/requirements.py
@@ -1,6 +1,22 @@
+"""Package requirements handling.
+
+Package requirements are strings defined in the ``dependencies`` attribute of
+``Formula`` and ``Package`` objects.
+
+These strings express what's needed at runtime for the software package to
+work.
+
+Examples::
+    
+    platform:package==version
+    package==version
+    package
+
+"""
 import pkg_resources as pkgres
 
 from .exceptions import IpkgException
+from .platforms import Platform
 
 
 class InvalidRequirement(IpkgException):
@@ -8,31 +24,72 @@ class InvalidRequirement(IpkgException):
     MESSAGE = 'Invalid requirement: %s'
 
 
-class PackageRequirement(object):
+class Requirement(object):
 
-    def __init__(self, requirement, **kw):
+    def __init__(self, requirement):
+        if ':' in requirement:
+            platform, package = requirement.split(':', 1)
+            platform = Platform.parse(platform)
+        else:
+            package = requirement
+            platform = Platform.current()
+
+        self.__platform = platform
+
         try:
-            self.__requirement = pkgres.Requirement.parse(requirement)
+            self.__pkg_req = pkgres.Requirement.parse(package)
         except ValueError:
             raise InvalidRequirement(requirement)
 
-        self.name = self.__requirement.project_name
-
     def __eq__(self, other):
-        if isinstance(other, PackageRequirement):
+        if isinstance(other, Requirement):
             return str(self) == str(other)
         elif isinstance(other, basestring):
             try:
-                requirement = pkgres.Requirement.parse(other)
-            except ValueError:
+                requirement = Requirement(other)
+            except InvalidRequirement:
                 return False
             else:
                 return str(self) == str(requirement)
         else:
             return False
 
-    def __str__(self):
-        return str(self.__requirement)
+    @property
+    def name(self):
+        return self.__pkg_req.project_name
 
-    def is_satisfied_by(self, obj):
-        return obj.name == self.name and obj.version in self.__requirement
+    @property
+    def version(self):
+        return ''.join(op + ver for op, ver in self.__pkg_req.specs)
+
+    @property
+    def extras(self):
+        return self.__pkg_req.extras
+
+    @property
+    def platform(self):
+        return self.__platform
+
+    def __str__(self):
+        extras = '[' + ','.join(self.extras) + ']' if self.extras else ''
+        return '%s:%s%s%s' % (self.platform, self.name, extras, self.version)
+
+    def __repr__(self):
+        return 'Requirement(%r)' % str(self)
+
+    def satisfied_by(self, obj):
+        """Returns ``True`` if ``obj`` satisfies this ``Requirement``.
+
+        ``obj`` should be a ``Formula`` or a ``Package`` object.
+        """
+        if isinstance(obj, dict):
+            return obj.get('platform', 'any') == self.__platform and \
+                obj.get('name') == self.__pkg_req.project_name and \
+                obj.get('version') in self.__pkg_req
+        else:
+            return (not hasattr(obj, 'platform') or
+                    obj.platform == self.__platform) and \
+                hasattr(obj, 'name') and \
+                obj.name == self.__pkg_req.project_name and \
+                hasattr(obj, 'version') and \
+                obj.version in self.__pkg_req
--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from ipkg import requirements
+from ipkg.requirements import Requirement, InvalidRequirement
 
 
 class Package(object):
@@ -8,27 +8,28 @@ class Package(object):
     def __init__(self, name, version):
         self.name = name
         self.version = version
+        self.platform = 'any'
 
 
 class TestPackageRequirement(TestCase):
 
     def test(self):
-        req = requirements.PackageRequirement('foo >= 1.0, < 2')
-        self.assertTrue(req.is_satisfied_by(Package('foo', '1.0')))
-        self.assertFalse(req.is_satisfied_by(Package('foo', '2.0')))
-        self.assertFalse(req.is_satisfied_by(Package('foo', '0.42')))
+        req = Requirement('any:foo < 2')
+        self.assertEqual(req.platform, 'any')
+
+    def test_satisfied_by(self):
+        req = Requirement('foo >= 1.0, < 2')
+        self.assertTrue(req.satisfied_by(Package('foo', '1.0')))
+        self.assertFalse(req.satisfied_by(Package('foo', '2.0')))
+        self.assertFalse(req.satisfied_by(Package('foo', '0.42')))
+
 
     def test_raises(self):
-        self.assertRaises(requirements.InvalidRequirement,
-                          requirements.PackageRequirement, 'foo/bar > 42%')
-
-    def test_str(self):
-        string = 'foo'
-        req = requirements.PackageRequirement(string)
-        self.assertEqual(string, str(req))
+        self.assertRaises(InvalidRequirement,
+                          Requirement, 'foo/bar > 42%')
 
     def test_eq(self):
-        self.assertTrue(requirements.PackageRequirement('foo==1.0') ==
-                        requirements.PackageRequirement('foo == 1.0'))
-        self.assertTrue(requirements.PackageRequirement('foo==1.0')
-                        == 'foo == 1.0')
+        self.assertTrue(Requirement('foo==1.0') == Requirement('foo == 1.0'))
+        self.assertTrue(Requirement('foo==1.0') == 'foo == 1.0')
+        self.assertFalse(Requirement('foo') == 'fnioewf < &*')
+        self.assertFalse(Requirement('foo') == 1)

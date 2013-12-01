0a8f6a6ebdf64ce45213e6daa7b7f05fa9aa581e
Philippe Muller <philippe.muller@gmail.com>
2013-12-02 00:56:01 +0100

    support for requirement merge
    no longer use pkg_resources.Requirement because it is buggy

--- ipkg/requirements.py
+++ ipkg/requirements.py
@@ -13,15 +13,25 @@ Examples::
     package
 
 """
+import re
+import operator
+from collections import defaultdict
+
 import pkg_resources as pkgres
 
 from .exceptions import IpkgException
-from .platforms import Platform
+from .platforms import Platform, InvalidPlatform
+from .compat import basestring
 
 
 class InvalidRequirement(IpkgException):
 
     MESSAGE = 'Invalid requirement: %s'
+
+
+class ExclusiveVersionRequirements(InvalidRequirement):
+
+    MESSAGE = 'Exclusive version requirements for operator %s: %s'
 
 
 class Requirement(object):
@@ -34,17 +44,12 @@ class Requirement(object):
             package = requirement
             platform = Platform.current()
 
-        self.__platform = platform
-
-        try:
-            self.__pkg_req = pkgres.Requirement.parse(package)
-        except ValueError:
-            raise InvalidRequirement(requirement)
-
-        self.__hash = hash(str(platform)) ^ hash(self.__pkg_req)
+        self.platform = platform
+        self.name, self.extras, self.versions = parse(package)
 
     def __hash__(self):
-        return self.__hash
+        return hash(str(self.platform)) ^ hash(self.name) ^ \
+            hash(tuple(self.extras)) ^ hash(tuple(self.versions))
 
     def __eq__(self, other):
         if isinstance(other, Requirement):
@@ -59,28 +64,53 @@ class Requirement(object):
         else:
             return False
 
-    @property
-    def name(self):
-        return self.__pkg_req.project_name
+    def __make_extras_str(self, extras):
+        return '[' + ','.join(extras) + ']' if extras else ''
 
-    @property
-    def version(self):
-        return ','.join(op + ver for op, ver in self.__pkg_req.specs)
-
-    @property
-    def extras(self):
-        return self.__pkg_req.extras
-
-    @property
-    def platform(self):
-        return self.__platform
+    def __make_versions_str(self, versions):
+        operators = dict((op, s) for s, op in OPERATORS.items())
+        versions_str = []
+        for version_operator, version_tuple in versions:
+            version_parts = []
+            for version_part in version_tuple:
+                if version_part == '*final':
+                    break
+                elif version_part[0] == '*':
+                    version_parts.append(version_part[1:])
+                else:
+                    version_parts.append(str(int(version_part)))
+            operator = operators[version_operator]
+            version = '.'.join(version_parts)
+            versions_str.append(operator + version)
+        return ','.join(versions_str)
 
     def __str__(self):
-        extras = '[' + ','.join(self.extras) + ']' if self.extras else ''
-        return '%s:%s%s%s' % (self.platform, self.name, extras, self.version)
+        extras = self.__make_extras_str(self.extras)
+        versions = self.__make_versions_str(self.versions)
+        return '%s:%s%s%s' % (self.platform, self.name, extras, versions)
 
     def __repr__(self):
         return 'Requirement(%r)' % str(self)
+
+    def __add__(self, other):
+        if isinstance(other, basestring):
+            other = Requirement(other)
+        elif not isinstance(other, Requirement):
+            raise TypeError(other)
+        if self.name != other.name:
+            raise InvalidRequirement(other)
+        if self.platform != other.platform:
+            raise InvalidPlatform(other.platform)
+        extras_str = self.__make_extras_str(set(self.extras + other.extras))
+        versions_str = self.__make_versions_str(
+            set(self.versions + other.versions))
+        return Requirement('%s:%s%s%s' % (self.platform, self.name,
+                                          extras_str, versions_str))
+
+    def satisfied_by_version(self, version):
+        if isinstance(version, basestring):
+            version = pkgres.parse_version(version)
+        return all(op(version, v) for op, v in self.versions)
 
     def satisfied_by(self, obj):
         """Returns ``True`` if ``obj`` satisfies this ``Requirement``.
@@ -88,13 +118,180 @@ class Requirement(object):
         ``obj`` should be a ``Formula`` or a ``Package`` object.
         """
         if isinstance(obj, dict):
-            return obj.get('platform', 'any') == self.__platform and \
-                obj.get('name') == self.__pkg_req.project_name and \
-                obj.get('version') in self.__pkg_req
+            return obj.get('platform', 'any') == self.platform and \
+                obj.get('name') == self.name and \
+                self.satisfied_by_version(obj.get('version'))
         else:
             return (not hasattr(obj, 'platform') or
-                    obj.platform == self.__platform) and \
+                    obj.platform == self.platform) and \
                 hasattr(obj, 'name') and \
-                obj.name == self.__pkg_req.project_name and \
+                obj.name == self.name and \
                 hasattr(obj, 'version') and \
-                obj.version in self.__pkg_req
+                self.satisfied_by_version(obj.version)
+
+
+RE_REQUIREMENT = re.compile("""
+^
+\s*
+(?P<name>[A-Za-z0-9_\-]+)
+\s*
+(
+    \[
+    (?P<extras>[A-Za-z0-9_\-, ]+)
+    \]
+)?
+\s*
+(?P<versions>[A-Za-z0-9_\-,!=\>\<\. ]+)*
+\s*
+$
+""", re.X)
+RE_VERSION_REQUIREMENT = re.compile("""
+^
+\s*
+(?P<operator>==|!=|\<|\>|\<=|\>=)
+\s*
+(?P<version>[0-9A-Za-z_\-\.]+)
+\s*
+$
+""", re.X)
+
+OPERATORS = {
+    '==': operator.eq,
+    '!=': operator.ne,
+    '>': operator.gt,
+    '>=': operator.ge,
+    '<': operator.lt,
+    '<=': operator.le,
+}
+
+def parse_version(version_string):
+    match = RE_VERSION_REQUIREMENT.match(version_string)
+
+    if not match:
+        raise InvalidRequirementVersion(version_string)
+
+    version_dict = match.groupdict()
+
+    if version_dict['operator'] not in OPERATORS:
+        raise InvalidRequirementVersionOperator(version_dict['operator'])
+
+    return OPERATORS[version_dict['operator']], \
+        pkgres.parse_version(version_dict['version'])
+
+
+def parse(requirement):
+    requirement_match = RE_REQUIREMENT.match(requirement)
+
+    if not requirement_match:
+        raise InvalidRequirement(requirement)
+
+    requirement_dict = requirement_match.groupdict()
+
+    version_strings = []
+    if requirement_dict['versions']:
+        version_strings = re.split(r'\s*,\s*', requirement_dict['versions'])
+        if len(version_strings) == 1 and not version_strings[0]:
+            version_strings = []
+
+    version_selectors = remove_useless_version_selectors(map(parse_version,
+                                                             version_strings))
+
+    if requirement_dict['extras']:
+        extras = re.split(r'\s*,\s*', requirement_dict['extras'].strip())
+    else:
+        extras = []
+
+    return requirement_dict['name'], extras, version_selectors
+
+
+def remove_useless_version_selectors(version_selectors):
+    selectors = defaultdict(set)
+
+    for op, version in version_selectors:
+        selectors[op].add(version)
+
+    if operator.gt in selectors:
+        gt_ver = sorted(selectors[operator.gt])[-1]
+    else:
+        gt_ver = None
+
+    if operator.ge in selectors:
+        ge_ver = sorted(selectors[operator.ge])[-1]
+    else:
+        ge_ver = None
+
+    if operator.lt in selectors:
+        lt_ver = sorted(selectors[operator.lt])[0]
+    else:
+        lt_ver = None
+
+    if operator.le in selectors:
+        le_ver = sorted(selectors[operator.le])[0]
+    else:
+        le_ver = None
+
+    l_selector, g_selector = None, None
+
+    if le_ver:
+        if lt_ver:
+            if le_ver < lt_ver:
+                # <= 1, < 2
+                l_selector = operator.le, le_ver
+            else:
+                # <= 2, < 1
+                # <= 2, < 2
+                l_selector = operator.lt, lt_ver
+        else:
+            l_selector = operator.le, le_ver
+    elif lt_ver:
+        l_selector = operator.lt, lt_ver
+
+    if ge_ver:
+        if gt_ver:
+            if ge_ver <= gt_ver:
+                # >= 1, > 2
+                # >= 2, > 2
+                g_selector = operator.gt, gt_ver
+            else:
+                # >= 2, > 1
+                g_selector = operator.ge, ge_ver
+        else:
+            g_selector = operator.ge, ge_ver
+    elif gt_ver:
+        g_selector = operator.gt, gt_ver
+
+    if g_selector and l_selector:
+        g_op, g_ver = g_selector
+        l_op, l_ver = l_selector
+        if g_ver == l_ver:
+            if g_op == operator.ge and l_op == operator.le:
+                selectors[operator.eq].add(l_ver)
+                g_selector, l_selector = None, None
+            else:
+                raise ExclusiveVersionRequirements(g_selector, l_selector)
+        elif g_ver > l_ver:
+            raise ExclusiveVersionRequirements(g_selector, l_selector)
+
+    eq_ver = None
+    eq_ver_count = len(selectors[operator.eq])
+    if eq_ver_count == 1:
+        eq_ver = selectors[operator.eq].pop()
+    elif eq_ver_count > 1:
+        raise ExclusiveVersionRequirements(operator.eq,
+                                           list(selectors[operator.eq]))
+
+    result = []
+
+    if eq_ver:
+        if l_selector or g_selector:
+            raise ExclusiveVersionRequirements(operator.eq,
+                                               'incompatible with other version operators')
+        else:
+            result.append((operator.eq, eq_ver))
+
+    if g_selector:
+        result.append(g_selector)
+    if l_selector:
+        result.append(l_selector)
+
+    return result
--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
+import operator
 
-from ipkg.requirements import Requirement, InvalidRequirement
+from ipkg.requirements import Requirement, InvalidRequirement, \
+    RE_REQUIREMENT, RE_VERSION_REQUIREMENT, parse_version, parse, \
+    remove_useless_version_selectors, ExclusiveVersionRequirements
 
 
 class Package(object):
@@ -16,6 +19,10 @@ class TestPackageRequirement(TestCase):
     def test(self):
         req = Requirement('any:foo < 2')
         self.assertEqual(req.platform, 'any')
+
+    def test_hash(self):
+        self.assertEqual(hash(Requirement('foo >1,<2')),
+                         hash(Requirement('foo >1,<2')))
 
     def test_satisfied_by(self):
         req = Requirement('foo >= 1.0, < 2')
@@ -33,3 +40,194 @@ class TestPackageRequirement(TestCase):
         self.assertTrue(Requirement('foo==1.0') == 'foo == 1.0')
         self.assertFalse(Requirement('foo') == 'fnioewf < &*')
         self.assertFalse(Requirement('foo') == 1)
+
+    def test_complex_1(self):
+        req = Requirement('foo>1,>2')
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '1.5'}))
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '2'}))
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '2.1'}))
+        req = Requirement('foo>2,>1')
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '1.5'}))
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '2'}))
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '2.1'}))
+
+    def test_complex_2(self):
+        req = Requirement('foo<3,<2')
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '3'}))
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '2'}))
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '1'}))
+        req = Requirement('foo<2,<3')
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '3'}))
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '2'}))
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '1'}))
+
+    def test_complex_3(self):
+        req = Requirement('foo<3,<=3')
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '3'}))
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '1'}))
+        req = Requirement('foo<=3,<3')
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '3'}))
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '1'}))
+
+    def test_merge(self):
+        # should not merge requirements with other object types
+        self.assertRaises(TypeError, Requirement('foo').__add__, None)
+        # should not merge requirements for different packages
+        self.assertRaises(InvalidRequirement, Requirement('foo').__add__,
+                          Requirement('bar'))
+
+        req = Requirement('foo>1.1,>1') + Requirement('foo<3,<2')
+        self.assertTrue(req.satisfied_by({'name': 'foo', 'version': '1.5'}))
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '1'}))
+        self.assertFalse(req.satisfied_by({'name': 'foo', 'version': '2'}))
+
+
+class TestRegexes(TestCase):
+
+    def test_requirement(self):
+        self.assertEqual(RE_REQUIREMENT.match('foo').groupdict(),
+                         {'name': 'foo', 'extras': None, 'versions': None})
+        self.assertEqual(RE_REQUIREMENT.match('foo == 2').groupdict(),
+                         {'name': 'foo', 'extras': None, 'versions': '== 2'})
+        self.assertEqual(RE_REQUIREMENT.match('foo ==2').groupdict(),
+                         {'name': 'foo', 'extras': None, 'versions': '==2'})
+        self.assertEqual(RE_REQUIREMENT.match('foo ==2, >= 3').groupdict(),
+                         {'name': 'foo', 'extras': None, 'versions': '==2, >= 3'})
+        self.assertEqual(RE_REQUIREMENT.match('foo [foo, bar,a-b ]==2, >= 3').groupdict(),
+                         {'name': 'foo', 'extras': 'foo, bar,a-b ', 'versions': '==2, >= 3'})
+
+    def test_version(self):
+        self.assertEqual(RE_VERSION_REQUIREMENT.match('>1').groupdict(),
+                         {'operator':'>','version':'1'})
+        self.assertEqual(RE_VERSION_REQUIREMENT.match(' >= 1.0a42 ').groupdict(),
+                         {'operator':'>=','version':'1.0a42'})
+
+
+class TestParseVersion(TestCase):
+
+    def test(self):
+        self.assertEqual(parse_version('==1.0 '),
+                         (operator.eq, ('00000001', '*final')))
+        self.assertEqual(parse_version('> 4.0'),
+                         (operator.gt, ('00000004', '*final')))
+
+
+class TestParse(TestCase):
+
+    def test(self):
+        self.assertEqual(parse('foo==1.0'),
+                         ('foo', [], [(operator.eq, ('00000001', '*final'))]))
+        self.assertEqual(parse('foo[bar, blah]'),
+                         ('foo', ['bar', 'blah'], []))
+        self.assertEqual(parse('foo[wtf,bar]==1.0'),
+                         ('foo', ['wtf', 'bar'], [(operator.eq, ('00000001', '*final'))]))
+
+    def test_mix_versions(self):
+        self.assertEqual(parse('foo>1,<=2'),
+                         ('foo', [], [(operator.gt, ('00000001', '*final')),
+                                      (operator.le, ('00000002', '*final'))]))
+        self.assertEqual(parse('foo>1,>2'),
+                         ('foo', [], [(operator.gt, ('00000002', '*final'))]))
+
+
+class TestRemoveUselessVersionSelectors(TestCase):
+
+    def test_dupplicate(self):
+        a = [(operator.gt, ('00000001', '*final')),
+             (operator.gt, ('00000001', '*final'))]
+        b = [(operator.gt, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_overlap_eq_raises(self):
+        a = [(operator.eq, ('00000001', '*final')),
+             (operator.eq, ('00000002', '*final'))]
+        self.assertRaises(ExclusiveVersionRequirements,
+                          remove_useless_version_selectors, a)
+
+    def test_overlap_gt(self):
+        a = [(operator.gt, ('00000001', '*final')),
+             (operator.gt, ('00000002', '*final'))]
+        b = [(operator.gt, ('00000002', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_overlap_ge(self):
+        a = [(operator.ge, ('00000001', '*final')),
+             (operator.ge, ('00000002', '*final'))]
+        b = [(operator.ge, ('00000002', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_overlap_lt(self):
+        a = [(operator.lt, ('00000001', '*final')),
+             (operator.lt, ('00000002', '*final'))]
+        b = [(operator.lt, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_overlap_le(self):
+        a = [(operator.le, ('00000001', '*final')),
+             (operator.le, ('00000002', '*final'))]
+        b = [(operator.le, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_overlap_le_lt(self):
+        # <=1,<2    <=1
+        a = [(operator.le, ('00000001', '*final')),
+             (operator.lt, ('00000002', '*final'))]
+        b = [(operator.le, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+        # <=1,<1    <1
+        a = [(operator.le, ('00000001', '*final')),
+             (operator.lt, ('00000001', '*final'))]
+        b = [(operator.lt, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+        # <=2,<1    <1
+        a = [(operator.le, ('00000002', '*final')),
+             (operator.lt, ('00000001', '*final'))]
+        b = [(operator.lt, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_overlap_ge_gt(self):
+        # >=1,>2    >2
+        a = [(operator.ge, ('00000001', '*final')),
+             (operator.gt, ('00000002', '*final'))]
+        b = [(operator.gt, ('00000002', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+        # >=1,>1    >1
+        a = [(operator.ge, ('00000001', '*final')),
+             (operator.gt, ('00000001', '*final'))]
+        b = [(operator.gt, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+        # >=2,>1    >=2
+        a = [(operator.ge, ('00000002', '*final')),
+             (operator.gt, ('00000001', '*final'))]
+        b = [(operator.ge, ('00000002', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+
+    def test_exclusive_g_l_selectors(self):
+        # >1,<1
+        a = [(operator.gt, ('00000001', '*final')),
+             (operator.lt, ('00000001', '*final'))]
+        self.assertRaises(ExclusiveVersionRequirements,
+                          remove_useless_version_selectors, a)
+        # >=1,<1
+        a = [(operator.ge, ('00000001', '*final')),
+             (operator.lt, ('00000001', '*final'))]
+        self.assertRaises(ExclusiveVersionRequirements,
+                          remove_useless_version_selectors, a)
+        # >1,<=1
+        a = [(operator.gt, ('00000001', '*final')),
+             (operator.le, ('00000001', '*final'))]
+        self.assertRaises(ExclusiveVersionRequirements,
+                          remove_useless_version_selectors, a)
+        # >=1,<=1
+        a = [(operator.ge, ('00000001', '*final')),
+             (operator.le, ('00000001', '*final'))]
+        b = [(operator.eq, ('00000001', '*final'))]
+        self.assertEqual(remove_useless_version_selectors(a), b)
+        # >2,<1
+        # >=2,<1
+        # >=2,<=1
+        # >2,<=1
+        a = [(operator.gt, ('00000002', '*final')),
+             (operator.lt, ('00000001', '*final'))]
+        self.assertRaises(ExclusiveVersionRequirements,
+                          remove_useless_version_selectors, a)
